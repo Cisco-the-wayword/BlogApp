660a833c46ba91a570b1ec8549cdf3e0b3a876f6
Cisco <ciscophrenzy@gmail.com>
2023-07-22 07:44:06 -0600

    Update comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,10 @@
 class Comment < ApplicationRecord
+  belongs_to :post
+  belongs_to :author, class_name: 'User'
+
+  after_create :update_post_comments_counter
+
+  def update_post_comments_counter
+    post.update_comments_counter
+  end
 end
