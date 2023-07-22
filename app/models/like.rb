7e70473cee160ae4bb85886353b127c23c7473bf
Cisco <ciscophrenzy@gmail.com>
2023-07-22 07:34:41 -0600

    Update likes.rb

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,2 +1,10 @@
 class Like < ApplicationRecord
+  belongs_to :post
+  belongs_to :author, class_name: 'User'
+
+  after_create :update_post_likes_counter
+
+  def update_post_likes_counter
+    post.update_likes_counter
+  end
 end
