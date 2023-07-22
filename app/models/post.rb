8cb5ccb12db091ad9eddfd69dbe87ccdce9bc01b
Cisco <ciscophrenzy@gmail.com>
2023-07-22 09:18:09 -0600

    Fix linter errors

--- app/models/post.rb
+++ app/models/post.rb
@@ -9,7 +9,6 @@ class Post < ApplicationRecord
   validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
-
   def update_author_posts_counter
     author.update_posts_counter
   end
