dbf8ae06d6431f21962d54c158e3c4cc773b6447
Cisco <ciscophrenzy@gmail.com>
2023-07-31 14:09:49 -0600

    Update models

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,9 +5,11 @@ class Post < ApplicationRecord
 
   after_create :update_author_posts_counter
 
-  validates :title, presence: true, length: { maximum: 250 }
-  validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
-  validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :title, presence: true, allow_blank: false, length: { maximum: 250 }
+  validates :comments_counter, numericality: { only_integer: true, allow_nil: true },
+                               comparison: { greater_than_or_equal_to: 0, allow_nil: true }
+  validates :likes_counter, numericality: { only_integer: true, allow_nil: true },
+                            comparison: { greater_than_or_equal_to: 0, allow_nil: true }
 
   def update_author_posts_counter
     author.update_posts_counter
@@ -22,6 +24,6 @@ class Post < ApplicationRecord
   end
 
   def recent_comments
-    comments.order(created_at: :desc).limit(5)
+    comments.order(created_at: :desc).includes([:author]).limit(5)
   end
 end
