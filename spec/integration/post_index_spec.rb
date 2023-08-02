24681cf6a06135ae8fd76be39dca2d0c707518d1
Cisco <ciscophrenzy@gmail.com>
2023-08-01 20:42:03 -0600

    Update spec

--- spec/integration/post_index_spec.rb
+++ spec/integration/post_index_spec.rb
@@ -9,11 +9,13 @@ RSpec.describe 'Post Index', type: :system do
     User.create(name: 'Adnan', photo: 'https://unsplash.com/photos/ABC123', posts_counter: 5)
   end
 
-  # Assuming we have more than one post (greater than what fits on a single page)
-  let!(:posts) do
-    (1..10).map do |i|
-      Post.create(title: "Post #{i}", text: "This is post #{i}", author: user, comments_counter: 0, likes_counter: 0)
-    end
+  let!(:post) do
+    Post.create(title: 'My First Post', text: 'This is my first post', author: user, comments_counter: 0,
+                likes_counter: 0)
+  end
+
+  let!(:comment) do
+    Comment.create(text: 'This is my first comment', author: user, post:)
   end
 
   it "displays the user's profile picture on user posts page" do
@@ -33,53 +35,32 @@ RSpec.describe 'Post Index', type: :system do
 
   it 'displays a post title on user posts page' do
     visit user_posts_path(user_id: user.id)
-    expect(page).to have_content(posts.first.title)
+    expect(page).to have_content(post.title)
   end
 
   it 'displays a post text on user posts page' do
     visit user_posts_path(user_id: user.id)
-    expect(page).to have_content(posts.first.text)
+    expect(page).to have_content(post.text)
   end
 
   it 'displays the first comments on a post' do
     visit user_posts_path(user_id: user.id)
-    expect(page).to have_content(posts.first.comments.first.text)
+    expect(page).to have_content(comment.text)
   end
 
   it 'displays the number of comments on a post' do
     visit user_posts_path(user_id: user.id)
-    expect(page).to have_content(posts.first.comments_counter)
+    expect(page).to have_content(post.comments_counter)
   end
 
   it 'displays the number of likes on a post' do
     visit user_posts_path(user_id: user.id)
-    expect(page).to have_content(posts.first.likes_counter)
+    expect(page).to have_content(post.likes_counter)
   end
 
   it 'redirects to post show page when clicking on post title' do
     visit user_posts_path(user_id: user.id)
-    click_link posts.first.title
-    expect(page).to have_current_path(user_post_path(user_id: user.id, id: posts.first.id))
-  end
-
-  context 'pagination' do
-    it 'displays pagination links' do
-      visit user_posts_path(user_id: user.id)
-
-      # Assuming we use some visual indicator for pagination links (e.g., the class "pagination")
-      expect(page).to have_css('.pagination')
-    end
-
-    it 'navigates to the next page when clicking the "Next" link' do
-      visit user_posts_path(user_id: user.id)
-
-      # Assuming we use the text "Next" for the next page link
-      click_link 'Next'
-
-      expect(page).to have_current_path(user_posts_path(user_id: user.id, page: 2))
-      expect(page).to have_content(posts[5].title) # Assuming the 6th post is displayed on the second page
-    end
-
-    # Similarly, you can add a test for the "Previous" link and other pagination behavior if applicable.
+    click_link post.title
+    expect(page).to have_current_path(user_post_path(user_id: user.id, id: post.id))
   end
 end
--- spec/integration/user_spec.rb
+++ spec/integration/user_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe User, type: :system do
     User.create(
       name: 'Tom Jenkins',
       photo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTqI5QUajL66Kkwz9efY-xCyC_Bq97XyTvgPmndMocETt62-ps3VjrzLtWNrDq_Y5qa5fQ',
-      bio: 'I\'m Tom, and I thrive on the thrill of innovation and creativity!',
+      bio: 'I\'m Tom, and I thrive on the thrill of innovation and creativity.',
       posts_counter: 0
     )
   end
@@ -118,6 +118,74 @@ RSpec.describe User, type: :system do
       click_link tom.name
 
       expect(page).to have_current_path(toms_show_page_url)
+    end
+  end
+
+  context 'show page test group 1' do
+    subject { brian }
+
+    before do
+      first_post
+      second_post
+      third_post
+      visit user_path subject
+    end
+
+    it 'shows the user profile picture' do
+      profile_picture = find('img')
+
+      expect(profile_picture).to be_visible
+      expect(profile_picture['src']).to eq subject.photo
+    end
+
+    it 'shows the user\'s name' do
+      expect(page).to have_content(subject.name)
+    end
+
+    it 'shows the number of posts the user has written' do
+      expect(page).to have_content("#{subject.posts_counter} Post#{subject.posts_counter == 1 ? '' : 's'}")
+    end
+
+    it 'shows the user bio' do
+      expect(page).to have_content(subject.bio)
+    end
+
+    it 'shows the user\'s first three posts' do
+      expect(page).to have_content(subject.posts[0].title)
+      expect(page).to have_content(subject.posts[1].title)
+      expect(page).to have_content(subject.posts[2].title)
+    end
+
+    it 'shows a button that shows all the user\'s posts when clicked' do
+      expect(page).to have_link('See all posts')
+    end
+  end
+
+  context 'show page test group 2' do
+    subject { brian }
+
+    before do
+      first_post
+      second_post
+      third_post
+      visit user_path subject
+    end
+
+    it 'redirects to the show page of a post when it is clicked' do
+      first_post_url = "#{Capybara.app_host}/users/#{subject.id}/posts/#{subject.posts.first.id}"
+
+      click_link subject.posts.first.title
+
+      expect(page).to have_current_path(first_post_url)
+      expect(page).to have_content(subject.posts.first.title)
+      expect(page).to have_content(subject.posts.first.text)
+    end
+
+    it 'redirects to the post\'s index page when the \'See all posts\' link is clicked' do
+      all_posts_url = "#{Capybara.app_host}/users/#{subject.id}/posts"
+
+      click_link 'See all posts'
+      expect(page).to have_current_path(all_posts_url)
     end
   end
 end
