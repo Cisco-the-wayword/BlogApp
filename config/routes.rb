b38f5db004dffe92dac49bfd9bc1cb1296f2616c
Cisco <ciscophrenzy@gmail.com>
2023-08-03 05:58:25 -0600

    Update config

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root "users#index"
   resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show]
-    resources :posts, only: [:index, :show, :new, :create] do
-      resources :comments, only: [:new, :create]
+    resources :posts, only: [:index, :show, :new, :create, :destroy] do
+      resources :comments, only: [:new, :create, :destroy]
       resources :likes, only: [:new, :create]
     end
   end
