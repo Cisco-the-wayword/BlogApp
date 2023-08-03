033c4954b3d3451da248ad5507f5c9d6ddbea10e
Cisco <ciscophrenzy@gmail.com>
2023-08-03 12:50:15 -0600

    Fix linters

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -35,6 +35,6 @@ class Ability
     #   can :update, Article, :published => true
     #
     # See the wiki for details:
-    #https://github.com/CanCanCommunity/cancancan/wiki/Defining-Abilities
+    # https://github.com/CanCanCommunity/cancancan/wiki/Defining-Abilities
   end
 end
